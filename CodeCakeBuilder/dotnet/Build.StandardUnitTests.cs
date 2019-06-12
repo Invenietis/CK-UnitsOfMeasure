c72c0e4780d18a75dd3d016f2aeebef3963dd070
Kuinox <nicolas.vandeginste@signature-code.com>
2019-06-12 11:14:49 +0200

    Applied settings + new StandardUnitTest + upgraded NUnit.  (...) Fixed StandardUnitTest.

--- CodeCakeBuilder/dotnet/Build.StandardUnitTests.cs
+++ CodeCakeBuilder/dotnet/Build.StandardUnitTests.cs
@@ -4,8 +4,12 @@ using Cake.Common.Solution;
 using Cake.Common.Tools.DotNetCore;
 using Cake.Common.Tools.DotNetCore.Test;
 using Cake.Common.Tools.NUnit;
+using Cake.Core.IO;
+using CK.Text;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 
@@ -13,91 +17,105 @@ namespace CodeCake
 {
     public partial class Build
     {
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="globalInfo"></param>
+        /// <param name="testProjects"></param>
+        /// <param name="useNUnit264ForNet461">Will use nunit 264 for Net461 project if true.</param>
         void StandardUnitTests( StandardGlobalInfo globalInfo, IEnumerable<SolutionProject> testProjects )
         {
             string memoryFilePath = $"CodeCakeBuilder/UnitTestsDone.{globalInfo.GitInfo.CommitSha}.txt";
 
             void WriteTestDone( Cake.Core.IO.FilePath test )
             {
-                System.IO.File.AppendAllLines( memoryFilePath, new[] { test.ToString() } );
+                if( globalInfo.GitInfo.IsValid ) File.AppendAllLines( memoryFilePath, new[] { test.ToString() } );
             }
 
             bool CheckTestDone( Cake.Core.IO.FilePath test )
             {
-                bool done = System.IO.File.Exists( memoryFilePath )
-                            ? System.IO.File.ReadAllLines( memoryFilePath ).Contains( test.ToString() )
+                bool done = File.Exists( memoryFilePath )
+                            ? File.ReadAllLines( memoryFilePath ).Contains( test.ToString() )
                             : false;
-                if( done ) Cake.Information( "Test already done on this commit." );
+                if( done )
+                {
+                    if( !globalInfo.GitInfo.IsValid )
+                    {
+                        Cake.Information( "Dirty commit: tests are run again (base commit tests were successful)." );
+                        done = false;
+                    }
+                    else
+                    {
+                        Cake.Information( "Test already successful on this commit." );
+                    }
+                }
                 return done;
             }
 
-            var testDlls = testProjects.Select( p =>
-                         new
-                         {
-                             CSProjPath = p.Path,
-                             ProjectPath = p.Path.GetDirectory(),
-                             NetCoreAppDll21 = p.Path.GetDirectory().CombineWithFilePath( "bin/" + globalInfo.BuildConfiguration + "/netcoreapp2.1/" + p.Name + ".dll" ),
-                             NetCoreAppDll22 = p.Path.GetDirectory().CombineWithFilePath( "bin/" + globalInfo.BuildConfiguration + "/netcoreapp2.2/" + p.Name + ".dll" ),
-                             Net461Dll = p.Path.GetDirectory().CombineWithFilePath( "bin/" + globalInfo.BuildConfiguration + "/net461/" + p.Name + ".dll" ),
-                             Net461Exe = p.Path.GetDirectory().CombineWithFilePath( "bin/" + globalInfo.BuildConfiguration + "/net461/" + p.Name + ".exe" ),
-                         } );
-
-            foreach( var test in testDlls )
+            foreach( SolutionProject project in testProjects )
             {
-                var net461 = Cake.FileExists( test.Net461Dll )
-                                ? test.Net461Dll
-                                : Cake.FileExists( test.Net461Exe )
-                                    ? test.Net461Exe
-                                    : null;
-                if( net461 != null )
+                NormalizedPath projectPath = project.Path.GetDirectory().FullPath;
+                NormalizedPath binDir = projectPath.AppendPart( "bin" ).AppendPart( globalInfo.BuildConfiguration );
+                NormalizedPath objDir = projectPath.AppendPart( "obj" );
+                string assetsJson = File.ReadAllText( objDir.AppendPart( "project.assets.json" ) );
+                bool isNunitLite = assetsJson.Contains( "NUnitLite" );
+                bool isVSTest = assetsJson.Contains( "Microsoft.NET.Test.Sdk" );
+                foreach( NormalizedPath buildDir in Directory.GetDirectories( binDir ) )
                 {
-                    Cake.Information( $"Testing via NUnit (net461): {net461}" );
-                    if( !CheckTestDone( net461 ) )
+                    string framework = buildDir.LastPart;
+                    string fileWithoutExtension = buildDir.AppendPart( project.Name );
+                    string testBinariesPath = "";
+                    if( isNunitLite )
                     {
-                        Cake.NUnit( new[] { net461 }, new NUnitSettings()
+                        //we are with nunitLite
+                        testBinariesPath = fileWithoutExtension + ".exe";
+                        if( File.Exists( testBinariesPath ) )
+                        {
+                            Cake.Information( $"Testing via NUnitLite ({framework}): {testBinariesPath}" );
+                            if( CheckTestDone( testBinariesPath ) ) return;
+                            Cake.NUnit3( new[] { testBinariesPath }, new NUnit3Settings
+                            {
+                                Results = new[] { new NUnit3Result() { FileName = FilePath.FromString( projectPath.AppendPart( "TestResult.Net461.xml" ) ) } }
+                            } );
+                        }
+                        else
+                        {
+                            testBinariesPath = fileWithoutExtension + ".dll";
+                            Cake.Information( $"Testing via NUnitLite ({framework}): {testBinariesPath}" );
+                            if( CheckTestDone( testBinariesPath ) ) return;
+                            Cake.DotNetCoreExecute( testBinariesPath );
+                        }
+                    }
+                    if(isVSTest)
+                    {
+                        testBinariesPath = fileWithoutExtension + ".dll";
+                        //VS Tests
+                        Cake.Information( $"Testing via VSTest ({framework}): {testBinariesPath}" );
+                        if( CheckTestDone( testBinariesPath ) ) return;
+                        Cake.DotNetCoreTest( projectPath, new DotNetCoreTestSettings()
+                        {
+                            Configuration = globalInfo.BuildConfiguration,
+                            Framework = framework,
+                            NoRestore = true,
+                            NoBuild = true,
+                            Logger = "trx"
+                        } );
+                    }
+
+                    if(!isVSTest && !isNunitLite)
+                    {
+                        testBinariesPath = fileWithoutExtension + ".dll";
+                        Cake.Information( "Testing via NUnit: {0}", testBinariesPath );
+                        Cake.NUnit( new[] { testBinariesPath }, new NUnitSettings()
                         {
                             Framework = "v4.5",
-                            ResultsFile = test.ProjectPath.CombineWithFilePath( "TestResult.Net461.xml" )
+                            ResultsFile = FilePath.FromString(projectPath.AppendPart( "TestResult.Net461.xml" ))
                         } );
-                        WriteTestDone( net461 );
-                    }
-                }
-                if( Cake.FileExists( test.NetCoreAppDll21 ) )
-                {
-                    TestNetCore( test.CSProjPath.FullPath, test.NetCoreAppDll21, "netcoreapp2.1" );
-                }
-                if( Cake.FileExists( test.NetCoreAppDll22 ) )
-                {
-                    TestNetCore( test.CSProjPath.FullPath, test.NetCoreAppDll22, "netcoreapp2.2" );
-                }
-            }
 
-            void TestNetCore( string projectPath, Cake.Core.IO.FilePath dllFilePath, string framework )
-            {
-                var e = XDocument.Load( projectPath ).Root;
-                if( e.Descendants( "PackageReference" ).Any( r => r.Attribute( "Include" )?.Value == "Microsoft.NET.Test.Sdk" ) )
-                {
-                    Cake.Information( $"Testing via VSTest ({framework}): {dllFilePath}" );
-                    if( CheckTestDone( dllFilePath ) ) return;
-                    Cake.DotNetCoreTest( projectPath, new DotNetCoreTestSettings()
-                    {
-                        Configuration = globalInfo.BuildConfiguration,
-                        Framework = framework,
-                        NoRestore = true,
-                        NoBuild = true,
-                        Logger = "trx"
-                    } );
+                    }
+                    WriteTestDone( testBinariesPath );
                 }
-                else
-                {
-                    Cake.Information( $"Testing via NUnitLite ({framework}): {dllFilePath}" );
-                    if( CheckTestDone( dllFilePath ) ) return;
-                    Cake.DotNetCoreExecute( dllFilePath );
-                }
-                WriteTestDone( dllFilePath );
             }
         }
-
     }
 }
